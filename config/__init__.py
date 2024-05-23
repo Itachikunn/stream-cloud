c74a3c3b180d70459af6d72417797b2339c9d1c2
Itachikunn <160922473+Itachikunn@users.noreply.github.com>
2024-05-23 16:33:48 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -4,6 +4,6 @@ class Config:
     API_ID = int( os.getenv("api_id","24339011") )
     API_HASH = os.getenv("api_hash","a85d1d917af0d4d02811a9a007b8dcda")
     CHANNEL = int( os.getenv("channel_files_chat_id","-1002193727359") )
-    CHANNEL_USERNAME = os.getenv("channel_username","itachixstream")
+    CHANNEL_USERNAME = os.getenv("channel_username","MergelogItachi")
     TOKEN = os.getenv("token","6990344734:AAHbfsLefdj-7I2TW6A10XWYfGkUVV89IWo")
     DOMAIN  = os.getenv("domain","https://itachixstream-1b0460f7986a.herokuapp.com/")
